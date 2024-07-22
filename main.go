d33bb2c27b5718306ae57493e99999b04f56f012
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2024-07-22 14:31:18 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,73 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("Server started on : http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println("error: ", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println("error: ", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
